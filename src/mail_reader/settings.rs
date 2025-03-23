2b9af4f10504aa64ba6c05cf26d622a28c1d77d3
VitalyOlegovic <vitalij_zad@libero.it>
2025-03-23 19:53:35 +0100

    feat: listing email messages to the terminal

---
+++ src/mail_reader/settings.rs
@@ -0,0 +1,47 @@
+use serde::Deserialize;
+
+use std::fs::File;
+use std::io::BufReader;
+use serde_yaml::Error;
+use backtrace::Backtrace;
+
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    pub imap_server: String,
+    pub port: u16,
+    pub email_address: String,
+}
+
+pub fn load_settings() -> Result<Settings, Error> {
+    // Open the YAML file
+    let file = File::open("src/resources/settings.yaml");
+    let file = match file {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        
+            // Capture and print the backtrace
+            let backtrace = Backtrace::new();
+            eprintln!("Backtrace:\n{:?}", backtrace);
+            panic!("Cannot find settings")
+        }
+    };
+
+    let reader = BufReader::new(file);
+
+    // Parse the YAML file into the Settings struct
+    let settings= serde_yaml::from_reader(reader);
+    let settings: Settings = match settings {
+        Ok(settings) => settings,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        
+            // Capture and print the backtrace
+            let backtrace = Backtrace::new();
+            eprintln!("Backtrace:\n{:?}", backtrace);
+            panic!("Cannot deserialize settings")
+        }
+    };
+
+    Ok(settings)
+}
